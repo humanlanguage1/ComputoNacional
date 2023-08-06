fcbd2a5cdbaa2e9a0d374653e443347850cf5aab
humanlanguage1 <diego_linares3@usmp.pe>
2023-08-06 15:03:50 -0500

    confiuguración bd

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'computo_smartnet1402',
+        'USER': 'postgres',
+        'PASSWORD': 'magdalena149',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
