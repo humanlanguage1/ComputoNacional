d0b870b86ae9bf55f866a2a3038173113a3bb054
humanlanguage1 <diego_linares3@usmp.pe>
2023-08-07 15:30:05 -0500

    actualizacion de forms y urls

--- inventario/inventario/urls.py
+++ inventario/inventario/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import static
+
 
 urlpatterns = [
     
+   # path('inventarioComputo/', include('inventarioComputo.urls')),
     path('inventarioComputo/', include('inventarioComputo.urls')),
     path('admin/', admin.site.urls),
 ]
--- inventario/inventarioComputo/urls.py
+++ inventario/inventarioComputo/urls.py
@@ -7,6 +7,6 @@ app_name = 'inventarioComputo'
 urlpatterns = [
     path('', views.index,  name='index'),
     path('registro',views.registro, name='registro'),
-    path('login', views.loginUsuario, name='login'),
-    path('producto/<int:producto_id>',views.producto,name='producto'),    
+    path('login', views.loginUsuario, name='login'), 
+    path('producto/<int:producto_id>',views.producto,name='producto'),
 ]
