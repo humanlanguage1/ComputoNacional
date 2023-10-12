d1f2881e877891c9252e7a6480bad75eb1653541
humanlanguage1 <diego_linares3@usmp.pe>
2023-10-11 23:01:02 -0500

    added static root

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_FILES_DIR = [BASE_DIR/"static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
