55e2b3550325de90e6fce0d243f8916ca678bd0c
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-07 11:22:30 -0500

    debug condition

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -130,8 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIR = [BASE_DIR/"static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
+else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
