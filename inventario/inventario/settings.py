7c231182128ff1666ee4f3bc2f70e3db9a626b2e
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-07 11:37:57 -0500

    static route

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if DEBUG:
-        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static/FrWork')]
+        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 else:
         STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
