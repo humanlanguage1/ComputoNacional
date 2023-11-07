f9ce8061d4e204d2137c9f7b63af8c720a76f7d2
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-06 20:13:15 -0500

    static root

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_FILES_DIR = [BASE_DIR/"staticfiles"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
