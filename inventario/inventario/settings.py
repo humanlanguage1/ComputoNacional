9b7088af2fa056ed2e91b00a54d1d1c13461d351
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-06 20:17:47 -0500

    static root

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_FILES_DIR = [BASE_DIR/"static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'static/'
 
 
 # Default primary key field type
