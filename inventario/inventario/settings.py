a4ad4a57ac1421efa8068a711e072c6259730670
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-11 12:15:49 -0500

    allowed hsots

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c2gy#ma9fbiqs^lxewk!n3put3r%2#6qb#*uekw42m6&$n)&5^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ '127.0.0.1' ]
+ALLOWED_HOSTS = ['3.83.246.178' , '127.0.0.1' ]
 
 
 # Application definition
