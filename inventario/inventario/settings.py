4c2321e62e5917d58b036b435af39f371eeede88
humanlanguage1 <diego_linares3@usmp.pe>
2023-11-14 17:05:09 -0500

    allowed host

--- inventario/inventario/settings.py
+++ inventario/inventario/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c2gy#ma9fbiqs^lxewk!n3put3r%2#6qb#*uekw42m6&$n)&5^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.212.27.88' , '127.0.0.1' ]
+ALLOWED_HOSTS = ['54.221.39.57' , '127.0.0.1' ]
 
 
 # Application definition
